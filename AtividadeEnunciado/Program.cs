9cf2bf697ffab1e4990a1795c77ee418b98bb331
KevinLsilva <kevinleasil2002@gmail.com>
2021-11-26 16:42:46 -0300

    First commit

---
+++ AtividadeEnunciado/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AtividadeEnunciado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
